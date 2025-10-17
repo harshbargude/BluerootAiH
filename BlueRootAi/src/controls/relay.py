5cb6cd4ad792ddd7c1f39797e29616b5ae5d73c6
harshbargude <123341917+harshbargude@users.noreply.github.com>
2025-10-17 18:41:30 +0530

    Update relay.py

--- BlueRootAi/src/controls/relay.py
+++ BlueRootAi/src/controls/relay.py
@@ -1,9 +1,15 @@
-# from gpiozero import OutputDevice
+from gpiozero import OutputDevice
 
-# class Relay:
-#     def __init__(self, pin: int, active_high: bool = False):
-#         self.dev = OutputDevice(pin, active_high=active_high, initial_value=False)
-#     def on(self): self.dev.on()
-#     def off(self): self.dev.off()
-#     @property
-#     def is_on(self) -> bool: return bool(self.dev.value)
+class Relay:
+    def __init__(self, pin: int, active_high: bool = False):
+        self.dev = OutputDevice(pin, active_high=active_high, initial_value=False)
+
+    def on(self):
+        self.dev.on()
+
+    def off(self):
+        self.dev.off()
+
+    @property
+    def is_on(self) -> bool:
+        return bool(self.dev.value)
